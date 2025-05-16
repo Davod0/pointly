4a9dfe4c745a16d1c080d8653668fa019429bb1c
Davod <davodnikoyii@gmail.com>
2025-05-16 12:31:53 +0200

    Refactor PlanningPokerContainer layout and content; improve heading and spacing for better readability

--- app/components/PlanningPokerContainer.tsx
+++ app/components/PlanningPokerContainer.tsx
@@ -7,36 +7,34 @@ import FeatureList from "./FeatureList";
 export default function PlanningPokerContainer() {
 
     return (
-        <div className="flex flex-1">
-        <div className="mt-20 ml-25 flex flex-col items-start">
-          <Badge />
-          <div>
-            <h1
-              className="text-5xl font-serif font-semibold text-gray-900 mb-3"
-              style={{ letterSpacing: "-0.02em" }}
-            >
-              Easier estimation by Pointly
-            </h1>
-          </div>
-          <p className="text-lg text-gray-600 mb-8">
-            Use Pointly for task estimation during development sprints
-          </p>
-          <FeatureList
-              point3="Collaborate with your team remotely"
-              point4="Keep sprints on track with clear estimates"
-              point1="Improving estimation accuracy"
-              point2="Boosting team collaboration"
-          />
-          <StartButton title="Start Estimating" route="/planning-poker" />
-        </div>
-        <div>
-          <SmallPointCardContainer />
-        </div>
-          <section
-            id="features"
-            className="bg-white py-25"
-            >
-          </section>
-      </div>
+        <>
+            <div className="flex flex-1">
+                <div className="mt-15 ml-27 flex flex-col items-start ">
+                    <Badge />
+                    <div>
+                        <h1
+                            className="text-5xl font-serif font-semibold text-gray-900 leading-snug mb-2 -mt-3"
+                            style={{ letterSpacing: "-0.02em" }}
+                        >
+                            Pointly Makes <br />Easier Estimations
+                        </h1>
+                    </div>
+                    <p className="text-lg text-gray-600 mb-4">
+                        Use Pointly for task estimation during development sprints
+                    </p>
+                    <FeatureList
+                        point3="Collaborate with your team remotely"
+                        point4="Keep sprints on track with clear estimates"
+                        point1="Improving estimation accuracy"
+                        point2="Boosting team collaboration"
+                    />
+                    <StartButton title="Start Estimating" route="/planning-poker" />
+                </div>
+                <div>
+                    <SmallPointCardContainer />
+                </div>
+            </div>
+            <section className="bg-white py-23 -mt-13 flex justify-center"></section>
+        </>
     )
 }
