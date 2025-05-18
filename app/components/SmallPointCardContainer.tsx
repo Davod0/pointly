275a12a5f2000f86108d5f471a2bdc5b96b5db73
Davod <davodnikoyii@gmail.com>
2025-05-18 11:48:50 +0200

    Remove unused CSS module import from SmallPointCardContainer component

--- app/components/SmallPointCardContainer.tsx
+++ app/components/SmallPointCardContainer.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import SmallPointCard from "./SmallPointCard";
-import styles from "./smallPointCardContainer.module.css";
 
 function useResponsiveCardCount() {
   const [count, setCount] = useState(8);
