fab3f838ebd24d8a14941fa048055b39f0537da5
Davod <davodnikoyii@gmail.com>
2025-05-21 12:39:06 +0200

    Update Header component image and add new assets

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -23,15 +23,15 @@ export default function Header() {
 
   return (
     <header className="w-full flex items-center px-18 py-8">
-      <Link href="/home" className="flex items-center">
-        <Image
-          src="/pointly-badge.png"
-          alt="Pointly Badge"
-          width={70}
-          height={70}
-          className="rounded-full hover:opacity-90 transition"
-        />
-      </Link>
+        <Link href="/home" className="flex items-center">
+    <Image
+      src="/p.png"
+      alt="Pointly Badge"
+      width={90}
+      height={90}
+      className="rounded-full hover:opacity-90 transition shadow-md border border-gray-300 max-w-full h-auto"
+    />
+  </Link>
 
       <div className="flex items-center ml-[6cm]">
         {navOptions.map((option) => (
