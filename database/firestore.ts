908e7aeaf5a3f998425342aa2ce6aea4e01b5519
Davod <davodnikoyii@gmail.com>
2025-06-05 21:22:17 +0200

    Installed Firebase and created a file for Firestore and its configuration

---
+++ database/firestore.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: process.env.FS_API_KEY,
+  authDomain: process.env.FS_AUTH_DOMAIN,
+  projectId: process.env.FS_PROJECT_ID,
+  storageBucket: process.env.FS_STORAGE_BUCKET,
+  messagingSenderId: process.env.FS_MESSAGING_SENDER_ID,
+  appId: process.env.FS_APP_ID,
+  measurementId: process.env.FS_MEASUREMENT_ID,
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
