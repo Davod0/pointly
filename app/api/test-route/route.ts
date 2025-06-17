ba325dbabcdcab9f5b415d62a6d33d9e72e1bae1
Davod <davodnikoyii@gmail.com>
2025-06-17 20:00:22 +0200

    Remove unused request parameter from GET function in test-route

--- app/api/test-route/route.ts
+++ app/api/test-route/route.ts
@@ -1,16 +1,13 @@
 
 const messages: string[] = [];
 
-
-export async function GET(req: Request){
+export async function GET(){
 
     return new Response(JSON.stringify({ messages }), {
         status: 200,
         headers: { "Content-Type": "application/json" }
     });
 }
-
-
 
 export async function POST(req: Request) {
   const { name } = await req.json();
