c14f95a5c5e90857b935efd0aae265f874b3cc4b
Davod <davodnikoyii@gmail.com>
2025-12-15 12:23:35 +0100

    Refactor ESLint configuration to simplify imports and streamline setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-config-next/flat";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...next.coreWebVitals,
+  ...next.typescript,
 ];
-
-export default eslintConfig;
